aaff476bd277e1547d9a8ed6e6da218555ac33f7
FrancoArce <harodi316@gmail.com>
2018-08-24 02:40:47 -0300

    Se añadió´codigo a fichero HolaMundo

---
+++ HolaMundo.java
@@ -0,0 +1,10 @@
+/****
+Pa' que caxi no mah po oe ****/
+public class HolaMundo{
+    public static void main(String[] args){
+        
+        System.out.print("Hello World!!");
+        
+    }
+    
+}
